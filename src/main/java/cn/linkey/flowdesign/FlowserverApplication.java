dabd68b69c9643b9011c385437feee0a80722184
alibao <walkwithdream@163.com>
2021-01-15 17:17:41 +0800

    发布v1.1版本，对应版本号：osflow-designer_1_1_20210115 重构osflow-designer,规范流程定义接口(SpringBoot版本)

---
+++ src/main/java/cn/linkey/flowdesign/FlowserverApplication.java
@@ -0,0 +1,13 @@
+package cn.linkey.flowdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowserverApplication.class, args);
+    }
+
+}
